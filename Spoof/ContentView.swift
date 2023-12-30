3ae476d04c5762252831f1acd211563dc59d452a
Linuxe <angelren1220@gmail.com>
2023-12-30 09:56:11 -0500

    Initial Commit

---
+++ Spoof/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Spoof
+//
+//  Created by Linxue Ren on 2023-12-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spoof/SpoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoofApp.swift
+//  Spoof
+//
+//  Created by Linxue Ren on 2023-12-30.
+//
+
+import SwiftUI
+
+@main
+struct SpoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
